69777e4f187dd46454bb79ed0059c88416aa27eb
devenamulshikder <devenamulshikder@gmail.com>
2024-05-16 02:21:04 +0600

    portolio section setup okay

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,21 @@
 const Portfolio = () => {
   return (
-    <div className="p-4">
-      <h3>This is My Portfolio</h3>
+    <div className="max-w-6xl mx-auto mt-6 md:mt-10 lg:mt-16">
+      <h3 className="text-2xl md:text-4xl lg:text-5xl lora font-bold">Portfolio</h3>
+
+<div>
+
+{/* project screen short */}
+<div>
+  <img src="" alt="" />
+</div>
+
+{/* projects details */}
+<div>
+
+</div>
+</div>
+
     </div>
   );
 };
